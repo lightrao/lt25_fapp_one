6b02ef108fe8054ff61610ae506267f882e95a8e
lightrao <lightrao@hotmail.com>
2025-02-12 20:59:18 +0800

    event bus update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // main.dart
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'event_bus_instance.dart';
 import 'counter_event.dart';
@@ -8,6 +10,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +22,8 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterHomePage extends StatefulWidget {
+  const CounterHomePage({super.key});
+
   @override
   _CounterHomePageState createState() => _CounterHomePageState();
 }
@@ -25,7 +31,8 @@ class CounterHomePage extends StatefulWidget {
 class _CounterHomePageState extends State<CounterHomePage> {
   int _counterPublisher = 0;
   int _counterSubscriber = 0;
-  late var _subscription;
+  // late var _subscription;
+  late StreamSubscription<CounterEvent> _subscription;
 
   @override
   void initState() {
