67c3577498143e4b99184e4a1e1ca3c2541a41db
lightrao <lightrao@hotmail.com>
2025-02-15 11:36:07 +0800

    vibration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:vibration/vibration.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,11 +9,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Liquid Progress Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Vibration Example',
+      home: VibrationHome(),
+    );
+  }
+}
+
+class VibrationHome extends StatelessWidget {
+  const VibrationHome({super.key});
+
+  // Function to trigger device vibration
+  Future<void> _vibrateDevice() async {
+    // Check if the device supports vibration
+    if (await Vibration.hasVibrator()) {
+      // Vibrate for 500 milliseconds
+      Vibration.vibrate(duration: 500);
+      print("Device is vibrating.");
+    } else {
+      // If device does not support vibration, print a message to the console
+      print("Device does not support vibration.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vibration Example'),
       ),
-      home: const HomeScreen(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _vibrateDevice,
+          child: Text('Vibrate Device'),
+        ),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import app_settings
 import audio_session
 import audioplayers_darwin
+import device_info_plus
 import just_audio
 import package_info_plus
 import path_provider_foundation
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
